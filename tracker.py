d19e2ec9339f927437ddc637d3e99d7fa43476de
Shauna <smkravec@gmail.com>
2021-08-15 16:02:13 -0700

    initial code to rewrite

--- tracker.py
+++ tracker.py
@@ -1,14 +1,40 @@
 import datetime
 import os
 import time
+import json
 
 import torch
-from tensorboardX import SummaryWriter
 import wandb
 
-class WandBTracker:
-    def __init__(self, name=None, args=None):
-        wandb.init(project="prismata-test", config=args)
+class Tracker:
+    def __init__(self, entity=None, project=None, args=None):
+        pass
+
+    def add_histogram(self, tag, data, i):
+        pass
+
+    def add_dictionary(self, dict):
+        pass
+
+    def add_image(self, tag, value, i):
+        pass
+
+    def set_summary(self, key, value):
+        pass
+
+    def add_scalar(self, tag, value, i):
+        pass
+
+    def log_iteration_time(self, batch_size, i):
+        pass
+
+class WandBTracker(Tracker):
+    def __init__(self, entity=None, project=None, args=None):
+        super().__init__(entity,project,args)
+        wandb.init(entity=entity ,project=name, config=args)
+
+    def add_dictionary(self, dict):
+        wand.log(dict)
 
     def add_histogram(self, tag, data, i):
         if type(data) == torch.Tensor:
@@ -32,19 +58,20 @@ class WandBTracker:
         except AttributeError:
             self.last_time = time.time()
 
+    def set_summary(self, key, value):
+        wandb.run.summary[key] = value
 
-class ConsoleTracker:
-    def __init__(self, name=None, args=None):
-        pass
 
-    def add_histogram(self, tag, data, i):
+class ConsoleTracker(Tracker):
+    def __init__(self, entity=None, project=None, args=None):
+        super().__init__(entity,project,args)
         pass
 
     def add_scalar(self, tag, value, i):
         print(f"{i}  {tag}: {value}")
 
-    def add_image(self, tag, value, i):
-        pass
+    def add_dictionary(self, dict):
+        print(json.dumps(dict))
 
     def log_iteration_time(self, batch_size, i):
         """Call this once per training iteration."""
@@ -56,3 +83,6 @@ class ConsoleTracker:
                 print(f"{i}  samples-per-sec: {batch_size/dt}")
         except AttributeError:
             self.last_time = time.time()
+
+    def set_summary(self, key, value):
+        print(f"{key}: {value}")
